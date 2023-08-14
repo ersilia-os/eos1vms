0daf998ea3b4fb0d4bf5d6c870ed518e0e2c4909
simrantan <96899862+simrantan@users.noreply.github.com>
2023-08-13 17:13:50 -0700

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -22,18 +22,6 @@ class Chembl(object):
     def __init__(self):
         self.model_path = os.path.join(checkpoints_dir, "chembl_28_multitask.onnx")
         self.ort_session = rt.InferenceSession(self.model_path)
-        self._work_out_targets()
-
-    def _work_out_targets(self):
-        descs = self._calc_morgan_fp(Chem.MolFromSmiles(EXAMPLE))
-        ort_inputs = {self.ort_session.get_inputs()[0].name: descs}
-        preds = self.ort_session.run(None, ort_inputs)
-        preds = self._format_preds(preds, [o.name for o in self.ort_session.get_outputs()])
-        targets = []
-        for p in preds:
-            targets += [p[0]]
-        self.targets = sorted(targets)
-        self.target_idxs = dict((k, i) for i,k in enumerate(self.targets))
 
     def _calc_morgan_fp(self, mol):
         fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(
@@ -45,22 +33,32 @@ class Chembl(object):
     def _format_preds(self, preds, targets):
         preds = np.concatenate(preds).ravel()
         np_preds = [(tar, pre) for tar, pre in zip(targets, preds)]
-        dt = [('chembl_id','|U20'), ('pred', '<f4')]
+        dt = [('chembl_id', '|U20'), ('pred', '<f4')]
         np_preds = np.array(np_preds, dtype=dt)
         np_preds[::-1].sort(order='pred')
         return np_preds
 
     def calc(self, mols):
+        targets_list = []
         fps = []
+        
         for mol in mols:
             descs = self._calc_morgan_fp(mol)
             ort_inputs = {self.ort_session.get_inputs()[0].name: descs}
             preds = self.ort_session.run(None, ort_inputs)
             preds = self._format_preds(preds, [o.name for o in self.ort_session.get_outputs()])
-            fp = np.zeros(len(self.targets))
+            targets = []
+            for p in preds:
+                targets += [p[0]]
+            targets_list.append(sorted(targets))
+            fp = np.zeros(len(targets))
             for p in preds:
                 fp[self.target_idxs[p[0]]] = p[1]
-            fps += [fp]
+            fps.append(fp)
+        
+        self.targets = sorted(set(target for targets in targets_list for target in targets))
+        self.target_idxs = dict((k, i) for i, k in enumerate(self.targets))
+        
         X = np.array(fps)
         return X
 
