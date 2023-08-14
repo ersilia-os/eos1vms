ae65b5b91edc9aaa7be3be5d0d6f3c4fe5c94a97
simrantan <96899862+simrantan@users.noreply.github.com>
2023-08-13 20:43:53 -0700

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -38,8 +38,7 @@ class Chembl(object):
         dt = [('chembl_id','|U20'), ('pred', '<f4')]
         np_preds = np.array(np_preds, dtype=dt)
         np_preds[::-1].sort(order='pred')
-        predicted_targets = [p[0] for p in np_preds]
-        return predicted_targets
+        return np_preds
 
     def calc(self, mols):
         fps = []
