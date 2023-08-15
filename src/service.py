e66d4b02b48cca2351cab835b5a3d2dbe2642922
simrantan <96899862+simrantan@users.noreply.github.com>
2023-08-15 15:50:16 -0700

    Update service.py

--- src/service.py
+++ src/service.py
@@ -67,7 +67,7 @@ class Model(object):
             h = next(reader)
             R = []
             for r in reader:
-                R += [{"scores": [float(x) for x in r]}]
+                R += [{"scores": [str(x) for x in r]}]
         output = {
             'result': R,
             'meta': {'scores': h}
