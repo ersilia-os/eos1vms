675b5d8f579c77ae381eebaf1bde9c2ce4c7c8df
simrantan <96899862+simrantan@users.noreply.github.com>
2023-08-17 08:35:13 -0700

    Update service.py

--- src/service.py
+++ src/service.py
@@ -76,7 +76,7 @@ class Model(object):
             'meta': {'scores': h}
         }
         shutil.rmtree(tmp_folder)
-        return output
+        return R
 
 
 class Artifact(BentoServiceArtifact):
